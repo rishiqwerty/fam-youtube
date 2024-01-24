33a9113ccd8998a228cb827a79b4ba57562f2888
Rishav Sharma <rishishah907@gmail.com>
2024-01-24 23:48:04 +0530

    Adding search functionality

--- frontend/src/components/videoDetails.js
+++ frontend/src/components/videoDetails.js
@@ -2,18 +2,14 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { useParams, useNavigate } from 'react-router-dom';
-// import Header from './commanComponent/header';
+
 function VideoDetails() {
   const [data, setData] = useState([]);
-  const [pay, setPayData] = useState({ days_worked: 0, Total_payment: 0, absent_days: 0 });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [pageCount, setPageCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const [showModal, setShowModal] = useState(false);
-  const [currentUser, setCurrentUser] = useState('All');
-  const [listOfUsers, setUsersList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const a = new Date();
   const [dateFilter, setDate] = useState(
     {
@@ -25,17 +21,15 @@ function VideoDetails() {
   useEffect(() => {
     console.log("Heres")
     fetchData(currentPage);
-  }, [currentPage, dateFilter, currentUser]);
+  }, [currentPage, dateFilter]);
 
   const fetchData = async (page) => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const token = window.localStorage.getItem("token");
       const response = await axios.get(
-        `http://localhost:8000/api/video-data/?page=${page}`,
-        // ?${id ? 'employee_id=' + id + '&' : ''}${dateFilter.start && dateFilter.end ? 'attendance_start_date=' + dateFilter.start + '&attendance_end_date=' + dateFilter.end+'&' : ''}page=${page}
+        `http://localhost:8000/api/video-data/?${searchTerm?'search='+searchTerm+'&':''}page=${page}`,
         );
         if (response) {
           setData(response.data.results);
@@ -44,7 +38,6 @@ function VideoDetails() {
       setPageCount(Math.ceil(response.data.count / 5)); // Assuming your API provides total count
 
     } catch (error) {
-      console.log(">>>>>>", pay)
       console.error(error);
       setError(error);
     } finally {
@@ -58,34 +51,57 @@ function VideoDetails() {
     setCurrentPage(actualPage); // Adjust for zero-based indexing
   };
 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setCurrentPage(1)
+    fetchData(currentPage)
+  }
+
 
   return (
     <div>
 
       <div className="container mt-4">
-        <div className="row">
-          {data.map((item) => (
-            <div key={item.id} className="row-md-6 mb-4">
-              <a href={item.urls} className="text-decoration-none text-dark">
-                <div className="card">
-                  <div className="row g-0">
-                    {/* Uncomment the line below if thumbnails are available */}
-                    <div className="col-md-6">
-                      <img src={item.thumbnails.default.url} alt="Thumbnail" className="img-fluid" />
-                    </div>
-                    <div className="col-md-6">
-                      <div className="card-body">
-                        <h5 className="card-title">{item.video_title}</h5>
-                        <p className="card-text">{item.description}</p>
-                        <p className="card-text text-muted">{item.channel_name}</p>
+        <form onSubmit={handleSearchSubmit} className="mb-4">
+            <div className="row">
+              <div className="col-md-8 mb-3">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="form-control"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </div>
+              <div className="col-md-4">
+                <button type="submit" className="btn btn-primary">
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+          <div className="row">
+            {data.map((item) => (
+              <div key={item.id} className="row-md-6 mb-4">
+                <a href={item.urls} className="text-decoration-none text-dark">
+                  <div className="card">
+                    <div className="row g-0">
+                      <div className="col-md-6">
+                        <img src={item.thumbnails.default.url} alt="Thumbnail" className="img-fluid" />
+                      </div>
+                      <div className="col-md-6">
+                        <div className="card-body">
+                          <h5 className="card-title">{item.video_title}</h5>
+                          <p className="card-text">{item.description}</p>
+                          <p className="card-text text-muted">{item.channel_name}</p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </a>
-            </div>
-          ))}
-        </div>
+                </a>
+              </div>
+            ))}
+          </div>
       </div>
 
 
--- youtube_fetch/db_populate/views.py
+++ youtube_fetch/db_populate/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import VideoData
 from .serializers import VideoDataSerializer
+from rest_framework import filters
 
 
 # Create your views here.
 class VideoDataView(generics.ListAPIView):
     queryset = VideoData.objects.all()
     serializer_class = VideoDataSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["video_title", "description"]
